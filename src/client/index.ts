befb45288c2e3938bd51a1041f0265a0385586eb
zaqk <10659278+zaqk@users.noreply.github.com>
2022-05-17 20:55:08 -0400

    add metamask connect

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,4 +1,5 @@
 import { Application } from "./Application";
+import { ethers } from 'ethers';
 
 const app = new Application();
 app.interpolation = true;
@@ -8,11 +9,18 @@ app.interpolation = true;
 
 (window as any).app = app;
 
+const initialize = () => {
+    const provider = new ethers.providers.Web3Provider((window as any).ethereum, 'any')
+    provider.send('eth_requestAccounts', [])
+}
+
 // allow to resize viewport and renderer
 window.onresize = () => {
     app.viewport.resize(window.innerWidth, window.innerHeight);
     app.renderer.resize(window.innerWidth, window.innerHeight);
 }
+
+window.addEventListener('DOMContentLoaded', initialize)
 
 // toggle interpolation
 document.addEventListener("click", (e) => {
